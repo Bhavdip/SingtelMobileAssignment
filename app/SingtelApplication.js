f8af815c28e33acda4f852ef5c9e92dea466bd7d
Bhavdip <bhavdip.pathar@gmail.com>
2022-09-17 05:47:25 +0530

    Initial commit

---
+++ app/SingtelApplication.js
@@ -0,0 +1,9 @@
+import {Component} from 'react';
+import {Text} from 'react-native';
+
+class SingtelApplication extends Component {
+  render() {
+    return <Text>Singtel Application</Text>;
+  }
+}
+export default SingtelApplication;
---
+++ index.js
@@ -0,0 +1,9 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+import SingtelApplication from './app/SingtelApplication';
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => SingtelApplication);
